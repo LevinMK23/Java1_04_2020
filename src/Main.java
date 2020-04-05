f75856110ae3ecd339aebfe79fcfcfc910183350
lalka98 <62359290+lalka98@users.noreply.github.com>
2020-04-05 18:18:18 +0300

    Update Main.java
    Сделал домашнее задание

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,51 @@ public class Main {
     public static void main(String[] args) {
         //sout
         System.out.println("Hello world");
+        System.out.println(functionOne(24, 24, 26, 8));
+        System.out.println(trueOrFalse(6,8));
+        System.out.println(numberFive(5));
+        System.out.println(numberSix(-5));
+        yourName("Вася");
+        year(2020);
     }
 }
+public static int functionOne (int a, int b, int c, int d) {
+        int firstNumber = a * (b + (c / d));
+        return firstNumber;
+    }
+    public static boolean trueOrFalse (int a, int b){
+        if (a + b <= 20 && a + b >= 10){
+            return true;
+        } else {
+            return false;
+        }
+    }
+    public static String numberFive (int a){
+        if( a >= 0){
+            return a + " Больше нуля";
+        } else {
+            return a + " Меньше нуля";
+        }
+    }
+    public static boolean numberSix (int a){
+        if (a < 0){
+            return true;
+        } else {
+            System.out.println("Чтобы вернулось значение true необходимо ввести отрицательное число");
+            return false;
+        }
+    }
+    public static void yourName (String name){
+        System.out.println("Привет " + name + "!");
+    }
+    public static void year ( int thisYear) {
+        if ( thisYear % 400 == 0){
+            System.out.println("Год является високосным");
+        } else if (thisYear % 100 == 0){
+            System.out.println("Год является невисокосным");
+        } else if (thisYear % 4 == 0){
+            System.out.println("Год является високосным");
+        } else {
+            System.out.println("Год является невисокосным");
+        }
+    }
