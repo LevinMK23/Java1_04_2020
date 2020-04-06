aad2d50577b3d72f2154bebe1a376607763c8fcf
Dikker425 <dikker>
2020-04-06 22:59:25 +0300

    Test commit

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ public class Main {
     //psvm public static void main
     public static void main(String[] args) {
         //sout
-        System.out.println("Hello world");
+        System.out.println("int a");
     }
 }
