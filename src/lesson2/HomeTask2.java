c49c7925f7f4422d961849c362219deb46adb509
AlonaZ <6alenyshka9@gmail.com>
2020-04-09 15:01:01 +0300

    Home Task - lesson2

---
+++ src/lesson2/HomeTask2.java
@@ -0,0 +1,17 @@
+public class HomeTask2 {
+    public static void main(String[] args) {
+        task1();
+
+    }
+    public  static void task1(){
+        int aaa[] = {0,0,0,1,1,1,0,0,0};
+        for (int i = 0; i < aaa.length; i++) {
+            aaa[i] = 0 + 1;
+            aaa[i] = 1 - 1;
+            System.out.println(aaa[i]);
+        }
+
+    }
+
+
+}
