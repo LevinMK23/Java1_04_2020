520eb02b9a232dd583548bd2ca5c14ab8bf39196
Shangaraev Ildar <wigravy@gmail.com>
2020-04-12 16:50:13 +0500

    refactor task 1 and task 2 for lesson 3. Fix some bugs

--- src/lesson3/Task3_1.java
+++ src/lesson3/Task3_1.java
@@ -1,50 +1,49 @@
 package lesson3;
+
 import java.util.Scanner;
 
 
 public class Task3_1 {
 
     public static void main(String[] args) {
-    gameStart();
+        gameStart(true);
     }
 
 
-    private static void gameStart() {
-        Scanner userIn = new Scanner(System.in);
+    private static void gameStart(boolean choice) {
+        if (choice) {
+            Scanner userIn = new Scanner(System.in);
             System.out.println("Игра \"Угадай число\"");
-                System.out.println("Введи максимальное число: ");
-                int maxNumberGuessed = userIn.nextInt();
-                System.out.println("А теперь количество попыток: ");
-                int amountAttempt = userIn.nextInt();
-                guessTheNumber(amountAttempt, maxNumberGuessed);
+            System.out.println("Введи максимальное число: ");
+            int maxNumberGuessed = userIn.nextInt();
+            System.out.println("А теперь количество попыток: ");
+            int amountAttempt = userIn.nextInt();
+            guessTheNumber(amountAttempt, maxNumberGuessed);
+        } else {
+            System.out.println("Ну как хочешь :)");
+            System.exit(0);
+        }
     }
 
-    private static void guessTheNumber (int tryCount, int maxNumberGuessed) {
+    private static void guessTheNumber(int totalTryCount, int maxNumberGuessed) {
         int guessedNumber = (int) (Math.random() * maxNumberGuessed);
+        int tryCounter = totalTryCount;
         Scanner in = new Scanner(System.in);
-        System.out.printf("Загадывается целое число от 0 до %d. Количество попыток: %d. Введи число: ", maxNumberGuessed, tryCount);
-        while (tryCount > 0) {
-            tryCount--;
+        System.out.printf("Загадывается целое число от 0 до %d. Количество попыток: %d. Игра начинается! Да прибудет с тобой сила. ", maxNumberGuessed, totalTryCount);
+        while(tryCounter > 0) {
+            tryCounter--;
+            System.out.print("Введите число: ");
             int getUserNumber = in.nextInt();
             if (getUserNumber == guessedNumber) {
-                System.out.printf("Верно! Ты победил! Осталось попыток: %d. Хочешь сыграть ещё? Введи 1, если да: ", tryCount);
-                if (in.hasNextInt(1)) gameStart();
-                else {
-                    System.out.println("Ну как хочешь.");
-                    return;
-                }
-            } else if (tryCount == 0) {
-                break;
-
-            } else if (getUserNumber > guessedNumber) {
-                System.out.printf("Неверно. У тебя осталось %d попыток, загаданное число МЕНЬШЕ, попробуй ещё: ", tryCount);
+                System.out.printf("Верно! Ты победил! Осталось попыток: %d из %d. Хочешь сыграть ещё? Введи 1, если да: ", tryCounter, totalTryCount);
+                gameStart(in.nextInt() == 1);
+            } else if (tryCounter != 0){
+                System.out.printf("Неверно. Осталось попыток: %d. Загаданное число %s. ", tryCounter, (getUserNumber > guessedNumber ? "МЕНЬШЕ" : "БОЛЬШЕ"));
             } else {
-                System.out.printf("Неверно. У тебя осталось %d попыток, загаданное число БОЛЬШЕ, попробуй ещё: ", tryCount);
+                System.out.println("Неверно! У тебя закончились попытки, ты проиграл. Хочешь сыграть ещё? Введи 1, если да: ");
+                gameStart(in.nextInt() == 1);
             }
-        }
-        System.out.println("\nНеверно! У тебя закончились попытки, ты проиграл. Хочешь сыграть ещё? Введи 1, если да: ");
-        if (in.hasNextInt(1)) {
-            gameStart();
         }
     }
 }
+
