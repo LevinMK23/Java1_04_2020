0ffd632ca7a0c198408f0ff74e389abcc5d30670
Shangaraev Ildar <wigravy@gmail.com>
2020-04-26 00:13:51 +0500

    add homework for lesson 7

---
+++ src/lesson7/Homework7/Cat.java
@@ -0,0 +1,28 @@
+package lesson7.Homework7;
+
+import java.util.Random;
+
+public class Cat {
+    Random random = new Random();
+    private String name;
+    private int foodQuality;
+    private int satiety = random.nextInt(50) + 10;
+    public Cat (String name, int foodQuality) {
+        this.foodQuality = foodQuality;
+        this.name = name;
+    }
+
+    public void getSatiety () {
+        System.out.println(name + " want to eat! Satiety: " + satiety);
+    }
+
+    public void eat (Plate p) {
+        if (p.getFoodQuality() >= foodQuality && p.getFood() >= satiety) {
+            p.decreaseFood(satiety);
+        } else if (p.getFood() < satiety) {
+            System.out.println("The plate is almost empty! Hurry up!");
+        }  else {
+            System.out.println(name + " is don't want to eat it. He's an offense at you.");
+        }
+    }
+}
