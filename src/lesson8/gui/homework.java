ece0632ca332abab107abd09a1fc272e9b7faa70
Понкратов Александр <ponkratov@leasingsoft.com>
2020-05-01 19:51:05 +0300

    8-th homework

---
+++ src/lesson8/gui/homework.java
@@ -0,0 +1,8 @@
+package lesson8.gui;
+
+public class homework {
+    public static void main(String[] args) {
+        System.out.println("My homework will be on repo https://github.com/APonkratov/converter");
+        System.out.println("I'll change my PR when done");
+    }
+}
